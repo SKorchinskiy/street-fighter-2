9702708036f7cd4d72d2f2f5bd0db87e2c933952
SKorchinskiy <ddphyk@gmail.com>
2023-06-24 19:14:13 +0300

    Implemented UserService methods

--- services/userService.js
+++ services/userService.js
@@ -3,12 +3,46 @@ import { userRepository } from "../repositories/userRepository.js";
 class UserService {
   // TODO: Implement methods to work with user
 
-  search(search) {
-    const item = userRepository.getOne(search);
-    if (!item) {
+  create(userData) {
+    const user = userRepository.create(userData);
+    if (!user) {
       return null;
     }
-    return item;
+    return user;
+  }
+
+  search(params) {
+    const user = userRepository.getOne(params);
+    if (!user) {
+      return null;
+    }
+    return user;
+  }
+
+  searchAll() {
+    const users = userRepository.getAll();
+    if (!users) {
+      return null;
+    }
+    return users;
+  }
+
+  update(id, updateUser) {
+    const user = userRepository.getOne({ id });
+    if (!user) {
+      return null;
+    }
+    const updatedUser = userRepository.update(id, updateUser);
+    return updatedUser;
+  }
+
+  remove(id) {
+    const user = this.search({ id });
+    if (!user) {
+      return null;
+    }
+    userRepository.delete(id);
+    return user;
   }
 }
 
