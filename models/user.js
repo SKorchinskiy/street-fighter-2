da84ca6c63389c8baccca64ea5d03a714e662d39
SKorchinskiy <ddphyk@gmail.com>
2023-06-24 19:13:57 +0300

    Added user fields RegEX constraints

--- models/user.js
+++ models/user.js
@@ -1,10 +1,10 @@
 const USER = {
   id: "",
-  firstName: "",
-  lastName: "",
-  email: "",
-  phoneNumber: "",
-  password: "", // min 3 symbols
+  firstName: "^(?!s*$).+",
+  lastName: "^(?!s*$).+",
+  email: "^[a-zA-Z0-9.+-]+@gmail.com",
+  phoneNumber: `^\\+380[0-9]{9}$`,
+  password: "^.{3,}$", // min 3 symbols
 };
 
 export { USER };
