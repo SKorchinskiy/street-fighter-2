06ce70a050ef1994858289cd0a3915f5b01092af
SKorchinskiy <ddphyk@gmail.com>
2023-06-24 20:01:00 +0300

    Apply dirname

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import cors from "cors";
 import express from "express";
 import { initRoutes } from "./routes/routes.js";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 import "./config/db.js";
 
@@ -10,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/*", express.static("./client/build"));
+app.use(express.static(join(__dirname, "client", "build")));
 
 initRoutes(app);
 
