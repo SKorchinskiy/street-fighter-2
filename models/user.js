c237c0596b7a0cb4c87b7c11dce7bed1045af157
SKorchinskiy <ddphyk@gmail.com>
2023-06-25 23:44:26 +0300

    + update regex for user model fields

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 const USER = {
   id: "",
-  firstName: "^(?!s*$).+",
-  lastName: "^(?!s*$).+",
-  email: "^[a-zA-Z0-9.+-]+@gmail.com",
+  firstName: "^(?!s*$).+$",
+  lastName: "^(?!s*$).+$",
+  email: "^[a-zA-Z0-9.+-]+@gmail.com$",
   phoneNumber: `^\\+380[0-9]{9}$`,
   password: "^.{3,}$", // min 3 symbols
 };
